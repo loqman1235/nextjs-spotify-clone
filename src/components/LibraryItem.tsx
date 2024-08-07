5ae7d39bda602c6143532b285d5ad8153effb449
loqman1235 <tonyvito12@gmail.com>
2024-08-07 09:32:56 +0100

    commit

--- src/components/LibraryItem.tsx
+++ src/components/LibraryItem.tsx
@@ -14,9 +14,12 @@ const LibraryItem = ({
   name,
 }: LibraryItemProps) => {
   return (
-    <Link href="/" className="flex items-center gap-4 px-2.5 py-2">
+    <Link
+      href="/"
+      className="flex items-center gap-4 px-2.5 py-2 rounded-md hover:bg-foreground-light transition duration-300 hover:shadow-md"
+    >
       {/* COVER | ICON | AVATAR */}
-      <div className="w-12 h-12 rounded-md overflow-hidden bg-foreground-light"></div>
+      <div className="w-12 h-12 rounded-md overflow-hidden bg-foreground-lighter"></div>
       <div>
         <h4 className="tracking-tight capitalize text-[15px]">{name}</h4>
         <p className="text-sm text-secondary-text">Playlist</p>
