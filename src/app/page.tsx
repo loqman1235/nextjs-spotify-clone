23348e514286b8ac6ff7ad9eea95102141ce555d
loqman1235 <tonyvito12@gmail.com>
2024-08-07 11:49:55 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ const Home = () => {
     <div className="w-full h-screen p-2 flex">
       <Sidebar />
       {/* resizer */}
-      <div className="w-[1px] h-full cursor-grab px-1"></div>
+      <div className=" h-full cursor-grab px-1 py-4 flex items-center justify-center group/resizer">
+        <div className="w-px h-full bg-white/35 group-active/resizer:bg-white/70 rounded-full opacity-0 group-hover/resizer:opacity-100 transition duration-300"></div>
+      </div>
       <div className="flex-grow bg-foreground rounded-md">Main Area</div>
     </div>
   );
