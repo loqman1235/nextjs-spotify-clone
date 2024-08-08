29604cec20eb2d90261a496a5dc2a33c2c863420
loqman1235 <tonyvito12@gmail.com>
2024-08-08 11:39:57 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,71 @@
+"use client";
+
 import Sidebar from "@/components/Sidebar";
+import { useEffect, useRef } from "react";
 
 const Home = () => {
+  const resizerRef = useRef<HTMLDivElement>(null);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const isResizing = useRef(false);
+
+  const handleStartResizing = (e: React.MouseEvent) => {
+    if (!resizerRef.current) return;
+    isResizing.current = true;
+
+    resizerRef.current.style.cursor = "grabbing";
+    document.body.style.cursor = "grabbing";
+  };
+
+  useEffect(() => {
+    const handleResizing = (e: MouseEvent) => {
+      if (!isResizing.current || !resizerRef.current || !sidebarRef.current)
+        return;
+      console.log("resizing start");
+
+      resizerRef.current.style.cursor = "grabbing";
+      resizerRef.current.firstElementChild?.classList.add("opacity-100");
+      document.body.style.cursor = "grabbing";
+
+      let newWidth = e.clientX;
+
+      if (newWidth > 823) newWidth = 823;
+      if (newWidth < 200) newWidth = 72;
+
+      sidebarRef.current.style.width = `${newWidth}px`;
+    };
+
+    const handleResizeStop = (e: MouseEvent) => {
+      if (!resizerRef.current) return;
+      if (isResizing.current) {
+        isResizing.current = false;
+      }
+
+      resizerRef.current.style.cursor = "grab";
+      resizerRef.current.firstElementChild?.classList.remove("opacity-100");
+      document.body.style.cursor = "default";
+    };
+
+    document.addEventListener("mousemove", handleResizing);
+    document.addEventListener("mouseup", handleResizeStop);
+
+    return () => {
+      document.removeEventListener("mousemove", handleResizing);
+      document.removeEventListener("mouseup", handleResizeStop);
+    };
+  }, []);
+
   return (
     <div className="w-full h-screen p-2 flex">
-      <Sidebar />
+      <Sidebar ref={sidebarRef} />
       {/* resizer */}
-      <div className=" h-full cursor-grab px-1 py-4 flex items-center justify-center group/resizer">
+      <div
+        onMouseDown={handleStartResizing}
+        ref={resizerRef}
+        className="h-full cursor-grab px-1 py-4 flex items-center justify-center group/resizer"
+      >
         <div className="w-px h-full bg-white/35 group-active/resizer:bg-white/70 rounded-full opacity-0 group-hover/resizer:opacity-100 transition duration-300"></div>
       </div>
-      <div className="flex-grow bg-foreground rounded-md">Main Area</div>
+      <div className="flex-grow bg-foreground rounded-md select-none"></div>
     </div>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import React, { forwardRef, Ref, useImperativeHandle } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { HomeIcon, LibraryIcon, SearchIcon } from "@/components/icons";
@@ -24,106 +23,113 @@ const links = [
 ] as const;
 
 const navBtnStyles =
-  "px-3 py-1.5 shadow-md rounded-full bg-foreground-lighter flex items-center justify-center text-[13px] font-medium hover:bg-foreground-lighter-hover transition duration-300";
+  "px-3 py-1.5 shadow-md rounded-full bg-foreground-lighter flex items-center justify-center text-[13px] font-medium hover:bg-foreground-lighter-hover transition duration-300 ";
 
-const Sidebar = () => {
-  const currentPath = usePathname();
+const Sidebar = forwardRef<HTMLDivElement>(
+  (props, ref: Ref<HTMLDivElement>) => {
+    const currentPath = usePathname();
 
-  return (
-    <div className="w-[314px] h-full flex flex-col gap-2">
-      {/* SIDEBAR TOP */}
-      <div className="bg-foreground p-3 rounded-lg">
-        <ul>
-          {links.map((link) => (
-            <li key={link.text} className="p-3">
-              <Link
-                href={link.href}
-                className={`flex items-center gap-5 hover:text-primary-text hover:fill-primary-text  transition duration-300 ${
-                  currentPath === link.href
-                    ? "text-primary-text fill-primary-text"
-                    : "text-secondary-text fill-secondary-text"
-                }`}
-              >
-                <span className={`text-3xl`}>
-                  {currentPath === link.href ? (
-                    <link.activeIcon size={24} />
-                  ) : (
-                    <link.icon size={24} />
-                  )}
-                </span>
+    return (
+      <div
+        ref={ref}
+        className="w-[314px] h-full flex flex-col gap-2 transition-[width] select-none"
+      >
+        {/* SIDEBAR TOP */}
+        <div className="bg-foreground p-3 rounded-lg">
+          <ul>
+            {links.map((link) => (
+              <li key={link.text} className="p-3">
+                <Link
+                  href={link.href}
+                  className={`flex items-center gap-5 hover:text-primary-text hover:fill-primary-text  transition duration-300 ${
+                    currentPath === link.href
+                      ? "text-primary-text fill-primary-text"
+                      : "text-secondary-text fill-secondary-text"
+                  }`}
+                >
+                  <span className={`text-3xl`}>
+                    {currentPath === link.href ? (
+                      <link.activeIcon size={24} />
+                    ) : (
+                      <link.icon size={24} />
+                    )}
+                  </span>
+                  <span className="capitalize text-[15px] font-semibold tracking-tight">
+                    {link.text}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* SIDEBAR BOTTOM */}
+        <div className="bg-foreground flex-1 rounded-md">
+          {/* HEADER */}
+          <div className="w-full py-3">
+            {/* HEADER TOP */}
+            <div className="flex items-stretch justify-between px-4 pb-5">
+              <button className="flex items-center gap-3 text-secondary-text hover:text-primary-text transition duration-300 pl-2">
+                <LibraryIcon width={24} height={24} />
                 <span className="capitalize text-[15px] font-semibold tracking-tight">
-                  {link.text}
+                  your library
                 </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {/* SIDEBAR BOTTOM */}
-      <div className="bg-foreground flex-1 rounded-md">
-        {/* HEADER */}
-        <div className="w-full py-3">
-          {/* HEADER TOP */}
-          <div className="flex items-stretch justify-between px-4 pb-5">
-            <button className="flex items-center gap-3 text-secondary-text hover:text-primary-text transition duration-300 pl-2">
-              <LibraryIcon width={24} height={24} />
-              <span className="capitalize text-[15px] font-semibold tracking-tight">
-                your library
-              </span>
-            </button>
-
-            <div className="flex items-center gap-2">
-              <button className="text-secondary-text hover:bg-foreground-light flex items-center justify-center p-1 rounded-full hover:text-primary-text transition duration-300">
-                <GoPlus size={24} />
               </button>
-              <button className="text-secondary-text hover:bg-foreground-light flex items-center justify-center p-1 rounded-full hover:text-primary-text transition duration-300">
-                <GoArrowRight size={24} />
+
+              <div className="flex items-center gap-2">
+                <button className="text-secondary-text hover:bg-foreground-light flex items-center justify-center p-1 rounded-full hover:text-primary-text transition duration-300">
+                  <GoPlus size={24} />
+                </button>
+                <button className="text-secondary-text hover:bg-foreground-light flex items-center justify-center p-1 rounded-full hover:text-primary-text transition duration-300">
+                  <GoArrowRight size={24} />
+                </button>
+              </div>
+            </div>
+            <nav className="flex items-center gap-2 px-5">
+              <button className={navBtnStyles}>Playlists</button>
+              <button className={navBtnStyles}>Artists</button>
+              <button className={navBtnStyles}>Albums</button>
+            </nav>
+          </div>
+          {/* LIBRARY */}
+          <div className="w-full ">
+            {/* HEADER */}
+            <div className="flex items-center justify-between px-5 py-2">
+              <button className="text-secondary-text hover:bg-foreground-lighter flex items-center justify-center p-1.5 rounded-full hover:text-primary-text transition duration-300">
+                <GoSearch size={20} />
+              </button>
+
+              {/* FILTER */}
+              <button className="flex items-center gap-1 text-sm text-secondary-text hover:text-primary-text hover:scale-105 transition duration-200 pr-1">
+                <span>Recent</span>
+                <GoListUnordered size={20} />
               </button>
             </div>
-          </div>
-          <nav className="flex items-center gap-2 px-5">
-            <button className={navBtnStyles}>Playlists</button>
-            <button className={navBtnStyles}>Artists</button>
-            <button className={navBtnStyles}>Albums</button>
-          </nav>
-        </div>
-        {/* LIBRARY */}
-        <div className="w-full ">
-          {/* HEADER */}
-          <div className="flex items-center justify-between px-5 py-2">
-            <button className="text-secondary-text hover:bg-foreground-lighter flex items-center justify-center p-1.5 rounded-full hover:text-primary-text transition duration-300">
-              <GoSearch size={20} />
-            </button>
 
-            {/* FILTER */}
-            <button className="flex items-center gap-1 text-sm text-secondary-text hover:text-primary-text hover:scale-105 transition duration-200 pr-1">
-              <span>Recent</span>
-              <GoListUnordered size={20} />
-            </button>
-          </div>
-
-          {/* LIBRARY ITEMS */}
-          <div className="w-full px-2.5">
-            <LibraryItem
-              name="liked songs"
-              type="playlist"
-              thumbnail="/images/liked.png"
-            />
-            <LibraryItem
-              name="your episodes"
-              type="playlist"
-              thumbnail="/images/episodes.png"
-            />
-            <LibraryItem
-              name="WE DONT TRUST YOU"
-              type="album"
-              thumbnail="/images/album1.jpg"
-            />
+            {/* LIBRARY ITEMS */}
+            <div className="w-full px-2.5">
+              <LibraryItem
+                name="liked songs"
+                type="playlist"
+                thumbnail="/images/liked.png"
+              />
+              <LibraryItem
+                name="your episodes"
+                type="playlist"
+                thumbnail="/images/episodes.png"
+              />
+              <LibraryItem
+                name="WE DONT TRUST YOU"
+                type="album"
+                thumbnail="/images/album1.jpg"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+Sidebar.displayName = "Sidebar"; // Necessary for React.forwardRef
 
 export default Sidebar;
