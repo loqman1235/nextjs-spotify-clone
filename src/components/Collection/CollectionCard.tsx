dc49367d4d0afb917f467e6e4a91d5c2feaf8b1f
loqman1235 <tonyvito12@gmail.com>
2024-08-13 12:56:02 +0100

    chore: Create player component and style it

--- src/components/Collection/CollectionCard.tsx
+++ src/components/Collection/CollectionCard.tsx
@@ -20,7 +20,7 @@ const CollectionCard = ({
       className="w-full h-[280px] p-2.5 flex flex-col gap-2 rounded-md overflow-hidden hover:bg-foreground-light transition duration-300 group/collectionCard"
     >
       {/* THUMBNAIL */}
-      <div className="w-full aspect-square rounded-md overflow-hidden relative">
+      <div className="w-full aspect-square rounded-md overflow-hidden relative shadow-xl">
         <Image src={thumbnail} alt="album" fill className="object-cover" />
         <button className="absolute bottom-2 right-2 bg-accent text-black p-1 rounded-full items-center justify-center text-4xl shadow-md  transition duration-300 group-hover/ group-hover/collectionCard:translate-y-0 translate-y-2 opacity-0 group-hover/collectionCard:opacity-100">
           <MdPlayArrow />
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,77 @@
+import Image from "next/image";
+import Link from "next/link";
+import { GoPlusCircle } from "react-icons/go";
+import {
+  MdPlayArrow,
+  MdRepeat,
+  MdShuffle,
+  MdSkipNext,
+  MdSkipPrevious,
+} from "react-icons/md";
+
 const Player = () => {
-  return <div>Player</div>;
+  return (
+    <div className="p-2 flex items-center">
+      {/* LEFT SECTION (CURRENT PLAYING SONG)*/}
+      <div className="hidden md:flex items-center gap-4">
+        {/* COVER */}
+        <div className="w-14 h-14 overflow-hidden rounded-md bg-white/20">
+          <Image
+            src="/images/albums/drake-scorpion.jpg"
+            alt="God's Plan"
+            width={56}
+            height={56}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col">
+          <Link href="/" className="hover:underline text-[13px]">
+            God&apos;s Plan
+          </Link>
+          <Link
+            href="/"
+            className="text-xs text-secondary-text hover:underline"
+          >
+            Drake
+          </Link>
+        </div>
+        {/* LIKE SONG BUTTON */}
+        <button className="text-secondary-text hover:text-primary-text transition duration-300">
+          <GoPlusCircle />
+        </button>
+      </div>
+      {/* PLAYER CONTROLS */}
+      <div className="flex-1 space-x-5 flex items-center justify-center flex-col w-full gap-2">
+        <div className="flex items-center justify-center gap-5 text-2xl w-full">
+          <button className="text-secondary-text hover:text-primary-text transition duration-300 hover:scale-105">
+            <MdShuffle />
+          </button>
+          <button className="text-3xl text-secondary-text hover:text-primary-text transition duration-300 hover:scale-105">
+            <MdSkipPrevious />
+          </button>
+          <button className="p-1 bg-white text-black flex items-center justify-center rounded-full hover:scale-105">
+            <MdPlayArrow />
+          </button>
+          <button className="text-3xl text-secondary-text hover:text-primary-text transition duration-300 hover:scale-105">
+            <MdSkipNext />
+          </button>
+          <button className="text-secondary-text hover:text-primary-text transition duration-300 hover:scale-105">
+            <MdRepeat />
+          </button>
+        </div>
+        <div className="flex items-center w-[90%] md:w-1/2 justify-center gap-2">
+          <span className="text-xs text-secondary-text">00:00</span>
+          {/* PLAYER SLIDER */}
+          <div className="w-full bg-foreground-lighter h-1 rounded-full relative overflow-hidden">
+            <div className="absolute left-0 w-1/2 h-full bg-white rounded-full"></div>
+          </div>
+          <span className="text-xs text-secondary-text">00:00</span>
+        </div>
+      </div>
+      {/* RIGHT SECTION  */}
+      <div className="hidden md:flex">Volume here</div>
+    </div>
+  );
 };
 
 export default Player;
